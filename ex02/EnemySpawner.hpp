70847a7f21bce32936088ce25dec22f0d9082392
Kudoshi <brendenguan@live.com>
2026-08-09 17:01:52 +0800

    update: constructor fix for enemyspawner

--- ex02/EnemySpawner.hpp
+++ ex02/EnemySpawner.hpp
@@ -2,7 +2,7 @@
 
 class EnemySpawner {
   public:
-    EnemySpawner(int maxCount) { m_count = maxCount; m_spawned = 0; }
+    EnemySpawner(int maxCount) : m_count(maxCount), m_spawned(0) {}
     int spawn();
     int spawnedCount() const{ return m_spawned; }
   private:
